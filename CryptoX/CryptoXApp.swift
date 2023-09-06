59c990e7519b105871aca86525dc8641f975484e
alpay <calalli1903@gmail.com>
2023-09-06 14:48:10 +0400

    Initial Commit

---
+++ CryptoX/CryptoXApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoXApp.swift
+//  CryptoX
+//
+//  Created by Alpay Calalli on 06.09.23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
